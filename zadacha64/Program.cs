aa6d94e1265d673eaa59678dc276000bf8d46a9a
Eduard <levitov.eduard@gmail.com>
2023-01-14 18:41:30 +0200

    zadacha 64

---
+++ zadacha64/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+static void Func(int n)
+        {
+            if(n != 0)
+            {
+                Console.WriteLine(n);
+                Func(n - 1);
+            }
+        }
+        
+        
+
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            Func(n);
+
+            Console.ReadKey(true);
+       
